64e0b215478a06b2de0c5e48fae1d6e7f609fd43
Sabbir Hossain Hridoy <98183098+Hridu06@users.noreply.github.com>
2026-06-30 10:45:47 +0600

    DB

---
+++ Module 12/LibraryManagementSystem/LibraryManagementSystem/Data/LibraryDbContext.cs
@@ -0,0 +1,41 @@
+﻿using LibraryManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LibraryManagementSystem.Data
+{
+    public class LibraryDbContext : DbContext
+    {
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<BorrowRecord> BorrowRecords { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // ISBN must be unique
+            modelBuilder.Entity<Book>()
+                .HasIndex(b => b.ISBN)
+                .IsUnique();
+
+            // AvailableCopies <= TotalCopies
+            modelBuilder.Entity<Book>()
+                .ToTable(t =>
+                    t.HasCheckConstraint(
+                        "CK_Book_Copies",
+                        "[AvailableCopies] <= [TotalCopies]"));
+
+            // Default BorrowDate = Current DateTime
+            modelBuilder.Entity<BorrowRecord>()
+                .Property(b => b.BorrowDate)
+                .HasDefaultValueSql("GETDATE()");
+        }
+    }
+}
